16888bf105a9390a50718ea326b7ace4d21ada85
Avadh Mewada <avadh1008mewada@gmail.com>
2020-07-09 21:12:46 +0530

    add README

---
+++ UserDataDemo/Controllers/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  UserDataDemo
+//
+//  Created by Avadh Mewada on 6/30/20.
+//  Copyright © 2020 Avadh Mewada. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+    
+    
+}
